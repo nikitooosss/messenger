3f9bafee62a25adb3d0e4b27cac49311f8a4d5d4
Nikita Smantser <nikita.a.smantser@gmail.com>
2026-05-14 13:50:47 +0300

    refactor: add user and chat depends

--- src/api/core/__init__.py
+++ src/api/core/__init__.py
@@ -1,6 +1,8 @@
 from .password import hash_password, verify_password
 from .jwt import create_access_token, get_current_user
 from .deps import get_message_service
+from .deps import get_user_service
+from .deps import get_chat_service
 
 __all__ = [
         "hash_password", 
@@ -8,4 +10,6 @@ __all__ = [
         "create_access_token", 
         "get_current_user",
         "get_message_service",
+        "get_user_service",
+        "get_chat_service",
     ]
--- src/api/core/deps.py
+++ src/api/core/deps.py
@@ -1,7 +1,16 @@
 from fastapi import Depends
 
-from api.services import MessageService
 from api.database import get_db
+from api.services import ChatService, MessageService, UserService
 
-def get_message_service(db = Depends(get_db)):
+
+def get_message_service(db=Depends(get_db)):
     return MessageService(db)
+
+
+def get_user_service(db=Depends(get_db)):
+    return UserService(db)
+
+
+def get_chat_service(db=Depends(get_db)):
+    return ChatService(db)

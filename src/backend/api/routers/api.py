4d6a7fb50280859122e11c8263e53b4a167c14b7
Nikita Smantser <nikita.a.smantser@gmail.com>
2026-06-09 15:22:46 +0300

    refactor: add /api to all endpoint

--- src/backend/api/routers/api.py
+++ src/backend/api/routers/api.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
 
+from backend.ws.endpoint import router_ws
+
 from .auth import router_auth
 from .chat import router_chat
 from .chat_participant import router_chat_participant
 from .message import router_message
 from .user import router_user
-from backend.ws.endpoint import router_ws
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/api")
 api_router.include_router(router_user)
 api_router.include_router(router_chat)
 api_router.include_router(router_message)
